3483cde32908cf3b6664776207773a2f529df51a
kvssankar <kvs.sankar001@gmail.com>
2020-10-26 20:18:01 +0530

    node implmented

---
+++ app.js
@@ -0,0 +1,106 @@
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bp=require("body-parser");
+const app = express();
+app.use(bp.json());
+app.use(bp.urlencoded({
+    extended: true
+  }));
+
+app.use(express.static(__dirname + '/css'));
+//keys
+const db = "mongodb+srv://kvssankar:sankarvishnu23@cluster1.uacfw.mongodb.net/webproj?retryWrites=true&w=majority";
+
+var userSchema = new mongoose.Schema({
+    volunteer: String,
+    phone: String,
+    email: String,
+    city:String,
+    state:String,
+    zip:String,
+    profession:String,
+    days:String,
+    activities:String,
+    pastExperienxe:String
+});
+
+var contactSchema=new mongoose.Schema({
+    name:String,
+    email:String,
+    number:String,
+    subject:String,
+    matter:String
+})
+
+var user = mongoose.model("user", userSchema);
+var contact = mongoose.model("contact", contactSchema);
+
+//connect to mongo
+const connect = mongoose
+  .connect(db, { useFindAndModify: false })
+  .then(() => console.log("Mondo db connected...."))
+  .catch((err) => console.log(err));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname+"/index.html");
+});
+
+app.get("/volunteer", function (req, res) {
+    res.sendFile(__dirname+"/form.html");
+});
+
+app.post("/volunteer",function(req,res){
+    user.create({
+        volunteer: req.body.name,
+    phone: req.body.phone,
+    email: req.body.eaddress,
+    city:req.body.city,
+    state: req.body.state,
+    zip: req.body.zip,
+    profession: req.body.profession,
+    days:req.body.day,
+    activities:req.body.activity,
+    pastExperienxe:req.body.contact
+    },
+    function (err, yolo) {
+        if (err) {
+            console.log("DATA IS NOT PUSHED");
+        } else {
+            console.log("DATA HAS BEEN PUSHED");
+            res.sendFile(__dirname+"/index.html");
+        }
+    }
+    )
+});
+
+app.post("/contactus",function(req,res){
+    contact.create({
+        name: req.body.name,
+        phone: req.body.phone,
+        email: req.body.eaddress,
+        subject:req.body.subject,
+        matter: req.body.matter
+    },
+    function (err, yolo) {
+        if (err) {
+            console.log("DATA IS NOT PUSHED");
+        } else {
+            console.log("DATA HAS BEEN PUSHED");
+            res.sendFile(__dirname+"/index.html");
+        }
+    }
+    )
+});
+
+app.get("/riskpredictor",function(req,res){
+    res.sendFile(__dirname+"/riskpredictor.html");
+})
+
+app.get("/contactus",function(req,res){
+    res.sendFile(__dirname+"/contact.html");
+})
+
+app.listen(process.env.PORT || 5000, function () {
+    console.log("SERVER 8000 HAS STARTED");
+});
